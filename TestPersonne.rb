c270aaa4b5bb9d72668b94c91d17d6598b479f65
Franck Paszkowski <statoon54@gmail.com>
2019-04-30 18:23:54 +0200

    Same version

--- TestPersonne.rb
+++ TestPersonne.rb
@@ -1,44 +1,37 @@
 #!/usr/bin/env ruby
 class Personne
-  attr_reader :nom, :prenom # dynamic getter
-
-  def initialize(nom:, prenom:) # keywords arguments(hash compatible)
+  def initialize(nom, prenom)
     @nom = nom
     @prenom = prenom
   end
 
-  def to_s
-    "#{nom} #{prenom}"
-  end
+  def to_s; "#{@nom} #{@prenom}" end
 end
 
 class Etudiant < Personne
-  attr_reader :num # dynamic getter
   class << self; attr_accessor :nb_etudiant end
   @nb_etudiant = 0
 
-  def initialize(nom:, prenom:, num:)
-    super(nom: nom, prenom: prenom) # call parent method
+  def initialize(nom, prenom, num)
+    super(nom, prenom) # pass arguments to parent
     @num = num
-    self.class.nb_etudiant += 1 # ou Etudiant.nb_etudiant += 1
+    self.class.nb_etudiant += 1 # or Etudiant.nb_etudiant += 1
   end
 
-  def to_s
-    super + " Student #{num}"
-  end
+  def to_s; super + " Student #{@num}" end
 end
 
 # Une instance
-p = Personne.new(nom: 'Duchemin ', prenom: 'paul')
+p = Personne.new('Duchemin ', 'paul')
 puts p
 
 # Une Collection
-al1 = (1..10).map { |i| Personne.new(nom: "Duchemin#{i}", prenom: "paul#{i}") }
+al1 = (1..10).map { |i| Personne.new("Duchemin#{i}", "paul#{i}") }
 puts al1
 
 # Polymorphisme
 al2 = (1..10).map do |i|
-  i.even? ? Personne.new(nom: "Duchemin#{i}", prenom: "Paul#{i}")
-          : Etudiant.new(nom: "Durand#{i}", prenom: "jules#{i}", num: i)
+  i.even? ? Personne.new("Duchemin#{i}", "Paul#{i}")
+          : Etudiant.new("Durand#{i}", "jules#{i}", i)
 end
 puts al2; puts "Nombre d'étudiants: #{Etudiant.nb_etudiant}"
